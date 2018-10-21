df67866e0a316af6027a79345ba8e0bf0f28f803
Austin Shindlecker <ashindlecker@gmail.com>
2018-10-21 14:28:25 -0700

    Comments for the test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,6 @@
 import { AddNumbers } from '../src/'
 
+//Test to ensure that Jest is configured properly.
 describe('Sanity Test', () => {
     it('Should add two numbers', () => {
         expect(AddNumbers(5,6)).toEqual(11);
