215b9ee7729c1f4d1ee6aab8d94e1619a063369a
Austin Shindlecker <ashindlecker@sitelock.com>
2018-10-25 14:18:26 -0700

    Added jest flow-typings for flow support in test files.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,10 @@
+//@flow
+
 import { AddNumbers } from '../src/'
 
 //Test to ensure that Jest is configured properly.
 describe('Sanity Test', () => {
     it('Should add two numbers', () => {
-        expect(AddNumbers(5,6)).toEqual(11);
+        expect(AddNumbers(5, 6)).toEqual(11);
     })
 })
